a7c26e0eb40845efe6b9db3e8a13ce3bb57b1bea
Supachai Thongsong <6510110469@psu.ac.th>
2023-03-03 15:07:41 +0700

    สรัางฟังก์ชันดักจับตัวเลขให้ไม่เกิน100และไม่ต่ำกว่า1

---
+++ randintGame_client.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432    # The port used by the server
+
+
+def num_guess():
+
+    while True:
+            
+        number = int(input("Enter your number: "))
+        if number < 1 or number > 100:
+            raise ValueError("Number should between 1 and 100")
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
