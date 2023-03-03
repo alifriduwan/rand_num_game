6bdad0d34b676863183d596ce5b6c6a8371266f8
TUNLATORN JANTARA <6510110632@psu.ac.th>
2023-03-03 15:20:37 +0700

    ตรวจสอบค่าที่เป็นตัวเลขเท่านั้น

--- randintGame_client.py
+++ randintGame_client.py
@@ -7,11 +7,13 @@ PORT = 65432    # The port used by the server
 def num_guess():
 
     while True:
+        try:
+            number = int(input("Enter your number: "))
+            if number < 1 or number > 100:
+                raise ValueError("Number should between 1 and 100")
             
-        number = int(input("Enter your number: "))
-        if number < 1 or number > 100:
-            raise ValueError("Number should between 1 and 100")
-
+        except ValueError :
+            print(str(ValueError))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
