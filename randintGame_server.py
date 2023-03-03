7d982e481723fb59371b3b6f24d229027c995721
psu6510110004 <dragonslayertitle@gmail.com>
2023-03-03 14:41:54 +0700

    Game_Function

---
+++ randintGame_server.py
@@ -0,0 +1,28 @@
+import random
+
+def game_num_rand(conn, addr):
+    rand_num = random.randint(1, 100)
+    round_play = 0
+
+    while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            num_guess = int(data.decode())
+            round_play += 1
+            
+            if round_play > 20:
+                print("lose")
+                break
+            
+            if num_guess < rand_num:
+                print(f"Your num_guess of {num_guess} is too low")
+                
+            elif num_guess > rand_num:
+                print(f"Your num_guess of {num_guess} is too high")
+                
+            else:
+                print("win")
+                break
+
+    conn.close()
